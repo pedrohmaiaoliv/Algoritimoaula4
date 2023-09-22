6ee7cd5fc944b6457817dbfc9ded2e0298d312d1
pedrohmaiaoliv <pedrohmaiaoliv@gmail.com>
2023-09-22 19:39:09 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("Digite o valor do raio: ");
+            int raio = 10;
+
+        System.out.println(raio);
+            double pi = 3.14;
+            double area = 3.14 * raio * raio;
+
+        System.out.printf("area = %f", area);
+    }
+}
