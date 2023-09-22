0d273b615e950dcd6801a02ccf135f20854785d2
pedrohmaiaoliv <pedrohmaiaoliv@gmail.com>
2023-09-22 20:01:15 -0300

    Andamento da Sala de Aula

--- src/App.java
+++ src/App.java
@@ -2,12 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.print("Digite o valor do raio: ");
-            int raio = 10;
+            int raio = 2;
 
         System.out.println(raio);
-            double pi = 3.14;
+            
+            final double pi = 3.14;
             double area = 3.14 * raio * raio;
 
         System.out.printf("area = %f", area);
     }
 }
+// na declaração de variável voce pode ter 2 tipos: variável e constante, quando ela é constante voce digita na frente dela, o tipo "final"
